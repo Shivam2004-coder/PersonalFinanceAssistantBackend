cb3a2e72c0a9af6e5f6189dcf026060687799bca
Shivam2004-coder <shivamgit003@gmail.com>
2025-09-14 21:14:36 +0530

    removed the multer

--- routes/gemini.js
+++ routes/gemini.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const multer = require("multer");
 const { geminiSearch } = require("../controllers/geminiController");
 const { userAuth } = require("../middlewares/auth");
 
-const upload = multer(); // memory storage
 const router = express.Router();
 
-router.post("/gemini/search", userAuth, upload.single("pdf"), geminiSearch);
+router.post("/gemini/search", userAuth , geminiSearch);
 
 module.exports = router;
